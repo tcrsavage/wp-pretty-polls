ef9ad475770185b5d5ea534fd0424ad664bad26c
tcrsavage <tcrsavage@gmail.com>
2013-12-02 18:40:39 +1100

    use status header instead of manually mapping response codes

--- api/api.php
+++ api/api.php
@@ -12,15 +12,7 @@ function wppp_authenticate_admin_api_request() {
 
 function wppp_api_respond( $response_code, $message = null ) {
 
-	$headers = array(
-		200 => 'HTTP/1.0 200 OK',
-		201 => 'HTTP/1.0 201 Created',
-		204 => 'HTTP/1.0 204 No Content',
-		400 => 'HTTP/1.0 400 Bad Request',
-		403 => 'HTTP/1.0 403 Forbidden',
-	);
-
-	header( isset( $headers[$response_code] ) ? $headers[$response_code] : 'HTTP/1.0 500 Internal Server Error' );
+	status_header( $response_code );
 
 	if ( $message )
 		echo $message;
